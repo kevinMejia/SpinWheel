2ad503658813467d9afe936511a8937e022ab5c9
kevinMejia <kevin7ama@gmail.com>
2018-12-31 16:41:28 -0500

    Add files via upload

---
+++ names.js
@@ -0,0 +1,23 @@
+
+var StudentNames = {
+
+    first:
+        [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        ],
+     second:
+        [
+         "second 1",
+         "second 2",
+         "second 3",
+         "second 4",
+         "second 5",
+         "second 6",
+        ]
+
+}
